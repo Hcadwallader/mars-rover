0acc2f49443af7b161ea7d064a9a1d8b8e2e048b
Hcadwallader <helenschwan3@gmail.com>
2024-08-26 22:16:06 +0100

    update readme and a bit of refactoring

--- README.md
+++ README.md
@@ -12,3 +12,14 @@ To run the tests
 ```
 pytest
 ```
+
+## comments
+
+Things I would have done next:
+
+Note I'm using version 3.9.6 of python which doesn't support switch statements, if I had time I would have upgrade to improve readability of code
+
+I would put the utilites file in a seperate folder and add some tests around the method
+
+I would also have liked to add more error checking and handling unexpected input formats/types to make the programme more robust
+
--- robot.py
+++ robot.py
@@ -1,5 +1,7 @@
 from direction_utilities import is_robot_lost
 class Robot:
+    direction_list = ["N", "E", "S", "W"]
+    length_of_list = len(direction_list)
 
     def __init__(self, x, y, direction, grid_x, grid_y):
         self._x = x
@@ -34,51 +36,42 @@ class Robot:
         return self._grid_y
 
     def turn_left(self):
-        direction_list = ["N", "E", "S", "W"]
-        length_of_list = len(direction_list)
-        current_direction_index = direction_list.index(self.direction.strip(' '))
+        current_direction_index = self.direction_list.index(self.direction.strip(' '))
         if(not self._lost):
             if(current_direction_index >= 1):
-                self._direction = direction_list[(current_direction_index-1)]
+                self._direction = self.direction_list[(current_direction_index-1)]
             else:
-                self._direction = direction_list[(length_of_list-1)]
+                self._direction = self.direction_list[(self.length_of_list-1)]
 
     def turn_right(self):
-        direction_list = ["N", "E", "S", "W"]
-        length_of_list = len(direction_list)
-        current_direction_index = direction_list.index(self.direction)
+        current_direction_index = self.direction_list.index(self.direction.strip(' '))
         if(not self._lost):
-            if(current_direction_index <= (length_of_list-2)):
-                self._direction = direction_list[(current_direction_index+1)]
+            if(current_direction_index <= (self.length_of_list-2)):
+                self._direction = self.direction_list[(current_direction_index+1)]
             else:
-                self._direction = direction_list[0]
+                self._direction = self.direction_list[0]
 
     def move_forward(self):
         if not self._lost:
             if self._direction == "N":
                 new_y = self._y + 1
-                if is_robot_lost(new_y, self._grid_y):
-                    self._lost = True
-                else:
-                    self._y = new_y
-            if self._direction == "E":
+                new_x = self.x
+            elif self._direction == "E":
                 new_x = self._x + 1
-                if is_robot_lost(new_x, self._grid_x):
-                    self._lost = True
-                else:
-                    self._x = new_x
-            if self._direction == "S":
+                new_y = self._y
+            elif self._direction == "S":
                 new_y = self._y - 1
-                if is_robot_lost(new_y, self._grid_y):
-                    self._lost = True
-                else:
-                    self._y = new_y
-            if self.direction == "W":
+                new_x = self._x
+            elif self.direction == "W":
                 new_x = self._x - 1
-                if is_robot_lost(new_x, self._grid_x):
-                    self._lost = True
-                else:
-                    self._x = new_x
+                new_y = self._y
+
+            if is_robot_lost(new_x, self._grid_x) or is_robot_lost(new_y, self._grid_y):
+                self._lost = True
+            else:
+                self._y = new_y
+                self._x = new_x
+
 
     def print_status(self):
         if self._lost:
